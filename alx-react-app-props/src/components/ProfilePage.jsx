023b431c347f6e19079717c11589c94c7db77577
Aime Ishimwe <medicalmind07@gmail.com>
2024-11-10 00:50:09 +0200

    Added missing files

--- alx-react-app-props/src/components/ProfilePage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import UserInfo from './UserInfo'; // Import UserInfo component
-
-function ProfilePage() {
-  return <UserInfo />;  // Just render the UserInfo component without props
-}
-
-export default ProfilePage;
