507d866bac7cc61c85e0a1fabb493f0cbde0c92a
Aime Ishimwe <medicalmind07@gmail.com>
2024-11-10 00:41:54 +0200

    Added Alx-react-app-props

---
+++ alx-react-app-props/src/components/ProfilePage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import UserInfo from './UserInfo'; // Import UserInfo component
+
+function ProfilePage() {
+  return <UserInfo />;  // Just render the UserInfo component without props
+}
+
+export default ProfilePage;
---
+++ alx-react-app-props/src/components/UserInfo.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import UserDetails from './UserDetails';  // Import UserDetails component
+
+function UserInfo() {
+  return <UserDetails />;  // Just render UserDetails without props
+}
+
+export default UserInfo;
