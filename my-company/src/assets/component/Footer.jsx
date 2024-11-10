ded4bdcc231a9ce72a82efb9a98cc407447ecc28
Aime Ishimwe <medicalmind07@gmail.com>
2024-11-10 03:29:21 +0200

    Added my-company directory

---
+++ my-company/src/assets/component/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+      <footer style={{ backgroundColor: 'brown', padding: '10px', color: 'white', textAlign: 'center', marginTop: 'auto' }}>
+        &copy; 2024 My Company. All rights reserved.
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
