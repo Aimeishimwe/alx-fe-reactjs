a05673ee181623860c1a4e9b78818c902d855533
Aime Ishimwe <medicalmind07@gmail.com>
2024-11-16 23:47:29 +0200

    update RecipeStore file

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,18 +1,17 @@
-import { create } from 'zustand';
+import create from 'zustand';
 
 const useRecipeStore = create((set) => ({
-  recipes: [],
-  addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
-  updateRecipe: (updatedRecipe) =>
-    set((state) => ({
-      recipes: state.recipes.map((recipe) =>
-        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
-      ),
-    })),
-  deleteRecipe: (recipeId) =>
-    set((state) => ({
-      recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
-    })),
+  recipes: [], // Initial state for recipes
+  addRecipe: (recipe) => set((state) => ({ recipes: [...state.recipes, recipe] })),
+  deleteRecipe: (id) => set((state) => ({
+    recipes: state.recipes.filter((recipe) => recipe.id !== id),
+  })),
+  toggleDone: (id) => set((state) => ({
+    recipes: state.recipes.map((recipe) =>
+      recipe.id === id ? { ...recipe, done: !recipe.done } : recipe
+    ),
+  })),
+  setRecipes: (newRecipes) => set(() => ({ recipes: newRecipes })), 
 }));
 
 export default useRecipeStore;
