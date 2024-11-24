1b7d0fe2da1906dd4903725a9507228fdd03d535
Aime Ishimwe <medicalmind07@gmail.com>
2024-11-24 14:21:03 +0200

    Important updates made to associated files

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,20 +1,28 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-import About from "./components/About";
+import Login from "./components/Login";
 import Profile from "./components/Profile";
-import NotFound from "./components/NotFound";
 import BlogPost from "./components/BlogPost";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
+  const isAuthenticated = true; // Mock authentication, set to false to test redirection
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/profile/*" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/profile/*"
+          element={
+            <ProtectedRoute isAuthenticated={isAuthenticated}>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/blog/:id" element={<BlogPost />} />
-        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
--- react-router-advanced/src/components/Post.jsx
+++
@@ -1,8 +0,0 @@
-import { useParams } from "react-router-dom";
-
-function Post() {
-  const { postId } = useParams();
-  return <div>Viewing post {postId}</div>;
-}
-
-export default Post;
