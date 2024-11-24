ec7d22c9d89f3cae3d5cc502d2a7a375699a5e51
Aime Ishimwe <medicalmind07@gmail.com>
2024-11-24 12:22:07 +0200

    Added form-handling-react project

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import FormikForm from "./components/formikForm";
+
+// App component rendering the FormikForm
+function App() {
+  return (
+    <div>
+      <FormikForm />
+    </div>
+  );
+}
+
+export default App;
