5f7c7cee374acd6febbd960c28c7822cfc1f98f8
Aime Ishimwe <medicalmind07@gmail.com>
2024-12-15 07:51:10 +0200

    updated services to include neccessary changes

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_GITHUB_API_URL;
+const BASE_URL = "https://api.github.com";
 
 export const fetchUserData = async (username) => {
   try {
     const response = await axios.get(`${BASE_URL}/users/${username}`);
     return response.data;
   } catch (error) {
-    throw new Error("User not found");
+    throw new Error("Failed to fetch user data");
   }
 };
